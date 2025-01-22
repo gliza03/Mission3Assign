84da010f0c1b1da87d98adeb3a53d1f283fd5df8
gabe <gabrielliza012@gmail.com>
2025-01-22 14:21:04 -0700

    Add project files.

---
+++ FoodItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission3Assign
+{
+    internal class FoodItem
+    {
+        public string Name, Cat, Quantity, ExpDate;
+        public FoodItem(string name, string cat, string quantity, string expirationDate)
+        {
+            Name = name;
+            Cat = cat;
+            Quantity = quantity;
+            ExpDate = expirationDate;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using Mission3Assign;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Create a list of objects (food items) that consists of name, cat, quantity, expiration
+        bool exit = true;
+        List<FoodItem> list = new List<FoodItem>();
+        while (exit)
+        {            
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("1. Add food items");
+            Console.WriteLine("2. Delete food items");
+            Console.WriteLine("3. Print list of food items:");
+            Console.WriteLine("4. Exit the program");
+            Console.WriteLine("Please enter your option:");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            // Add food item
+            if (choice == 1)
+            {
+                Console.WriteLine("What's the name of the food item?");
+                string name = Console.ReadLine();
+                Console.WriteLine("What's the category of the food item?");
+                string category = Console.ReadLine();
+                Console.WriteLine("How many do you have?");
+                string quantity = Console.ReadLine();
+                Console.WriteLine("When does the food item expire?");
+                string expdate = Console.ReadLine();
+
+                FoodItem foodItem = new FoodItem(name, category, quantity, expdate);
+                list.Add(foodItem);
+
+            }
+
+            // Delete food item from list
+            else if (choice == 2)
+            {
+                Console.WriteLine("Which food item would you like to delete?");
+                for (int i = 0; i < list.Count; i++)
+                {
+                    Console.WriteLine((i+1).ToString() + ": " + list[i].Name);
+                }
+                int num = int.Parse(Console.ReadLine());
+                list.Remove(list[num-1]);
+            }
+
+            // Print list of items
+            else if (choice == 3)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    Console.WriteLine((i+1).ToString() + ": " + list[i].Name);
+                }
+            }
+
+            // Exit program
+            else if (choice == 4)
+            {
+                exit = false;
+            }
+
+        }
+    }
+}
